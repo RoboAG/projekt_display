8a9355866cd01b5825012ea4d738eff4fc66a231
Peter Weissig <peterweissig@github.com>
2018-04-01 17:23:11 +0200

    added travis job for software-tests

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # projekt_display
 Rotierendes Display
+
+[![Build Status](https://travis-ci.org/RoboAG/projekt_display.svg?branch=master)](https://travis-ci.org/RoboAG/projekt_display)
---
+++ travis/2_build_all.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+### travis file for building all software modules
+
+
+
+echo ""
+echo "### Sourcing robolib"
+. ../../robolib/scripts/bashrc.sh
+
+
+
+echo ""
+echo "### Building"
+cd ../Software/
+PATH_SOFTWARE="$(pwd)/"
+PATH_DEVEL="${PATH_SOFTWARE}devel/"
+
+
+
+echo ""
+echo "## Peter"
+PATH_PETER="${PATH_DEVEL}Peter/"
+
+echo "# test"
+cd ${PATH_PETER}test/
+#robolib_all
+if [ $? -ne 0 ]; then exit -1; fi
+
+
+
+echo ""
+echo "## Main Peter"
+PATH_MAIN_PETER="${PATH_SOFTWARE}PETER/"
+
+echo "# simple_text"
+cd ${PATH_MAIN_PETER}simple_text/
+robolib_all
+if [ $? -ne 0 ]; then exit -1; fi
